0c5f73fcc2538ab825e2334b62bd62d3145069b6
venkam <puthavenkam1234@gmail.com>
2025-01-05 18:17:18 +0530

    expense shell

--- frontend.sh
+++ frontend.sh
@@ -55,7 +55,7 @@ VALIDATE $? "Moving to HTML directory"
 unzip /tmp/frontend.zip &>>$LOG_FILE_NAME
 VALIDATE $? "unzipping the frontend code"
 
-cp /home/ec2-user/expense-shell/frontend.config /etc/nginx/default.d/frontend.config
+cp /root/expense-shell/frontend.config /etc/nginx/default.d/frontend.config
 VALIDATE $? "Copied frontend config"
 
 systemctl restart nginx &>>$LOG_FILE_NAME
